9f2441b21ed0a38896dd33222f87347dd7b2e145
Ekaterina <rfvb.foto@mail.ru>
2021-01-17 15:45:37 +0300

    new

--- app/src/main/java/ru/netologia/MainActivity.kt
+++ app/src/main/java/ru/netologia/MainActivity.kt
@@ -3,6 +3,7 @@ package ru.netologia
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import ru.netologia.adapter.PostsAdapter
 import ru.netologia.databinding.ActivityMainBinding
 import ru.netologia.viewmodel.PostViewModel
 
@@ -10,49 +11,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+                val binding = ActivityMainBinding.inflate(layoutInflater)
+                setContentView(binding.root)
 
-        val viewModel: PostViewModel by viewModels()
-        viewModel.data.observe(this) { post ->
-            with(binding) {
-                txtAuthor.text = post.author
-                txtPublished.text = post.published
-                txtContent.text = post.content
-                txtLikes.text = numbersString(post.likes)
-                txtShare.text = numbersString(post.share)
-
-                imgLike.setImageResource(
-                        if (post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_favorite_24
+                val viewModel: PostViewModel by viewModels()
+                val adapter = PostsAdapter(
+                    onLikeListener = {
+                        viewModel.likeById(it.id)
+                    },
+                    onShareListener = {
+                        viewModel.shareById(it.id)
+                    }
                 )
+                binding.listView.adapter = adapter
+                viewModel.data.observe(this) { posts ->
+                    adapter.list = posts
+                }
             }
         }
-        binding.imgLike.setOnClickListener {
-            viewModel.like()
-        }
-        binding.imgShare.setOnClickListener {
-            viewModel.share()
-        }
-    }
-
-    fun numbersString(number: Int): String {
-        return when (number) {
-            in 0..999 -> number.toString()
-            in 1_000..9_999 -> if ((number / 100) % 10 == 0) {
-                (number / 1_000).toString() + "K"
-            } else {
-                String.format("%.1f", number * 1.0 / 1_000) + "K"
-            }
-            in 10_000..999_999 -> (number / 1_000).toString() + "K"
-            else -> if ((number / 100_000) % 10 == 0) {
-                (number / 1000_000).toString() + "M"
-            } else {
-                String.format("%.1f", number * 1.0 / 1000_000) + "M"
-            }
-
-        }
-    }
-}
-
-
-
--- app/src/main/java/ru/netologia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netologia/viewmodel/PostViewModel.kt
@@ -4,9 +4,10 @@ import androidx.lifecycle.ViewModel
 import ru.netologia.repository.PostRepository
 import ru.netologia.repository.PostRepositoryInMemoryImpl
 
+
 class PostViewModel : ViewModel() {
     private val repository: PostRepository = PostRepositoryInMemoryImpl()
-    val data = repository.get()
-    fun like() = repository.like()
-    fun share() = repository.share()
+    val data = repository.getAll()
+    fun likeById(id: Long) = repository.likeById(id)
+    fun shareById(id: Long) = repository.shareById(id)
 }
