7da9a7348f4970e78695882223d2332b23856c7e
Ekaterina <rfvb.foto@mail.ru>
2021-02-04 19:41:27 +0300

    new

--- app/src/main/java/ru/netologia/MainActivity.kt
+++ app/src/main/java/ru/netologia/MainActivity.kt
@@ -3,8 +3,11 @@ package ru.netologia
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 import ru.netologia.AddNewPost.Companion.textArg
 
 
@@ -29,7 +32,26 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                     }
                 )
         }
+        checkGoogleApiAvailability()
     }
-}
+    private fun checkGoogleApiAvailability() {
+        with(GoogleApiAvailability.getInstance()) {
+            val code = isGooglePlayServicesAvailable(this@MainActivity)
+            if (code == ConnectionResult.SUCCESS) {
+                return@with
+            }
+            if (isUserResolvableError(code)) {
+                getErrorDialog(this@MainActivity, code, 9000).show()
+                return
+            }
+            Toast.makeText(
+                    this@MainActivity,
+                    "Google Api Unavailable",
+                    Toast.LENGTH_LONG
+            )
+                    .show()
+        }
+    }
 
+}
 
